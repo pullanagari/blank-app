823707bc44bf9abec4b1b5aec78ceabc1061731f
Reddy pullanagari <shekerreddy@hotmail.com>
2025-09-13 16:53:55 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
+import requests
 import streamlit as st
+
+token = st.secrets["GITHUB_TOKEN"]
+repo = st.secrets["GITHUB_REPO"]
+
+r = requests.get(f"https://api.github.com/repos/{repo}", headers={"Authorization": f"token {token}"})
+st.write(r.status_code, r.json())
 
 st.title("🎈 My new app")
 st.write(
